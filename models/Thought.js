465276cbc112911c8e0c61c4b4e81fa15cbad8aa
CChampness <87551272+CChampness@users.noreply.github.com>
2022-01-05 17:12:46 -0500

    fleshed out models for user and thought

--- models/Thought.js
+++ models/Thought.js
@@ -2,37 +2,37 @@ const { Schema, model } = require('mongoose');
 
 // Schema to create a thought model
 const thoughtSchema = new Schema(
-  // {
-  //   thoughtText: {
-  //     type: String,
-  //     required: true,
-  //     min_length: 1,
-  //     max_length: 280,
-  //   },
-  //   createdAt: {
-  //     type: Date,
-  //     default: Date.now(),
-  //     // need getter to format
-  //   },
-  //   username: {
-  //     type: String,
-  //     required: true,
-  //   },
-  //   // reactions: [reactionSchema],
-  //   reactions: [
-  //     {
-  //       type: Schema.Types.ObjectId, 
-  //       ref: "reaction"
-  //     }
-  //   ],
-  // },
-  // {
-  //   toJSON: {
-  //     virtuals: true,
-  //     getters: true,
-  //   },
-  //   id: false,
-  // }
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      // need getter to format
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    // reactions: [reactionSchema],
+    reactions: [
+      {
+        type: Schema.Types.ObjectId, 
+        ref: "reaction"
+      }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
 );
 
 // // Create a virtual called `reactionCount` that retrieves
